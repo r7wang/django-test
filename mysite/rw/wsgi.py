41cd269b953fd2c4e068c3c91ac5ef58e22acd8b
Ray Wang <r7wang@gmail.com>
2020-03-14 11:31:30 -0400

    Rename config directory to rw.

--- mysite/rw/wsgi.py
+++ mysite/rw/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mysite project.
+WSGI rw for rw project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rw.settings')
 
 application = get_wsgi_application()
