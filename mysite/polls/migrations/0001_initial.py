e1fd59eced17a8069a0164e3fb614b4213ceef1c
Ray Wang <r7wang@gmail.com>
2020-03-16 00:13:57 -0400

    Removed replacement migrations.

--- mysite/polls/migrations/0001_initial.py
+++ mysite/polls/migrations/0001_initial.py
@@ -6,8 +6,6 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
-    replaces = [('polls', '0001_initial'), ('polls', '0002_metadata'), ('polls', '0003_auto_20191128_1810'), ('polls', '0004_auto_20200316_0407')]
-
     initial = True
 
     dependencies = [
